758820f96142b4130334f22ddd70b68c0f564955
HARUKI <82438100+Haruki33086@users.noreply.github.com>
2024-01-25 12:34:13 +0900

    Update ImageSubscriber.cs

--- Assets/Scripts/ImageSubscriber.cs
+++ Assets/Scripts/ImageSubscriber.cs
@@ -1,20 +1,57 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections.Generic;
 
 public class ImageSubscriber : MonoBehaviour
 {
-    [SerializeField] string streamURL = "http://<your ip>:8080/simu/camera?_method=SUB";
-    [SerializeField] RawImage imageDisplay;
+    [SerializeField] private string streamURL = "http://13.208.62.139:8080/simu/camera?_method=SUB";
+    [SerializeField] private RawImage imageDisplay;
+    [SerializeField] private int textureWidth = 640; // RenderTextureの幅の初期値
+    [SerializeField] private int textureHeight = 480; // RenderTextureの高さの初期値
 
     private MJPEGStreamDecoder mjpegDecoder;
+
+    public int TextureWidth
+    {
+        get { return textureWidth; }
+        set
+        {
+            if (textureWidth != value)
+            {
+                textureWidth = value;
+                UpdateRenderTexture();
+            }
+        }
+    }
+
+    public int TextureHeight
+    {
+        get { return textureHeight; }
+        set
+        {
+            if (textureHeight != value)
+            {
+                textureHeight = value;
+                UpdateRenderTexture();
+            }
+        }
+    }
 
     void Start()
     {
         mjpegDecoder = gameObject.AddComponent<MJPEGStreamDecoder>();
-        mjpegDecoder.renderTexture = new RenderTexture(640, 480, 24);
-        imageDisplay.texture = mjpegDecoder.renderTexture;
+        UpdateRenderTexture();
         mjpegDecoder.StartStream(streamURL);
+    }
+
+    private void UpdateRenderTexture()
+    {
+        if (mjpegDecoder.renderTexture != null)
+        {
+            Destroy(mjpegDecoder.renderTexture);
+        }
+
+        mjpegDecoder.renderTexture = new RenderTexture(textureWidth, textureHeight, 24);
+        imageDisplay.texture = mjpegDecoder.renderTexture;
     }
 
     void OnDestroy()
